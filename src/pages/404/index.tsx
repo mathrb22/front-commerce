595d88ce188d0482fd2129b676756aff2f44bc47
Matheus Ribeiro <mathribe2020@gmail.com>
2022-09-30 02:01:22 -0300

    fix: export default 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export function Page404() {
+export default function Page404() {
 	return (
 		<>
 			<Head>
